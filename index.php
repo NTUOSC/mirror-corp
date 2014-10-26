194a331acbea68699dcaa943f5d834dfc365da4d
Shouko <h999342@gmail.com>
2014-10-27 04:22:05 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$filename = date("Ymd_His_U_").rand(100,999).'.txt';
+$content = file_get_contents('php://input');
+if(strlen($content) <= 524288){
+	file_put_contents($content, $filename);
+}
+?>
