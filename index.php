408a5f88952987b2b8968f601595740639565db6
Shouko <h999342@gmail.com>
2014-10-27 04:28:48 +0800

    Update syntax and error handling

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 $filename = date("Ymd_His_U_").rand(100,999).'.txt';
 $content = file_get_contents('php://input');
-if(strlen($content) <= 524288){
-	file_put_contents($content, $filename);
+if(strlen($content) <= 524288 && !empty($content)){
+	file_put_contents($filename, $content);
 }
 echo json_encode(
 	array(
